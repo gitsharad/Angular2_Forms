5760846a9301a8482d1f51f0ff6e0ad9bd9fdda4
Sharad K. Pawar <sharadp@india.inc>
2017-08-07 22:39:33 +0530

    first commit

---
+++ angular-example/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {AboutComponent} from './about/about.component';
+import {CareerComponent} from './career/career.component';
+import { ContactComponent } from "./contact/contact.component";
+import {HomeComponent} from "./home/home.component";
+import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ServicesComponent } from './services/services.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component:  HomeComponent },
+  { path: 'about',component:  AboutComponent },
+  { path: 'contact',component:ContactComponent },
+  { path: 'career',component: CareerComponent },
+   { path: 'services',component: ServicesComponent } 
+
+];
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    CareerComponent,
+    NavigationComponent,
+    ServicesComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+    ),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
