5df8b39728f0ad349156cf5f7274d353003a8d9c
Sharad K. Pawar <sharadp@india.inc>
2017-09-06 20:47:10 +0530

    8 th commit angular material design installation

--- angular-example/src/app/Components/skillsets/skillsets.component.ts
+++ angular-example/src/app/Components/skillsets/skillsets.component.ts
@@ -1,22 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { SkillsService } from "../../myservices/skills.service";
 import { Subscription } from 'rxjs/Subscription';
-import {MdProgressBarModule} from '@angular/material';
+import {MdSliderModule} from '@angular/material';
 @Component({
   selector: 'app-skillsets',
   templateUrl: './skillsets.component.html',
   styleUrls: ['./skillsets.component.css'],
-  providers:[MdProgressBarModule]
+  providers:[MdSliderModule]
 })
 export class SkillsetsComponent implements OnInit {
 
    subscription: Subscription;
    myskills: any;
-   color = 'primary';
-   mode = 'determinate';
-   value = 50;
-   bufferValue = 75;
-  constructor(private skillservice:SkillsService,private mdprogressbar:MdProgressBarModule) {
+   
+  constructor(private skillservice:SkillsService,private mdslidermodule:MdSliderModule) {
    
       this.subscription=this.skillservice.getSkills().subscribe(
         skills=>{
